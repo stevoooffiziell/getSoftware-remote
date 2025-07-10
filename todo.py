70743e537547a9545cd4d32bdf7343995ff0eb45
stevoooffiziell <112954334+stevoooffiziell@users.noreply.github.com>
2025-07-10 07:34:46 +0200

    updated todo‘s

--- todo.py
+++ todo.py
@@ -1,7 +1,15 @@
 # TODO:
 #   bug fixing:
-#       resolve following error
-#           Fehler beim Einfügen in Test-DB: ('42000', '[42000] [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Zeichenfolgen- oder Binärdaten werden in Tabelle "sw_inst.dbo.table_prod", Spalte "name" abgeschnitten. Abgeschnittener Wert: PTV Europe City Map Premium 2022.1H (C:\\Program Files (x86)\\PTVGROUP\\maps\\EuropePremium202. (2628) (SQLExecDirectW); [42000] [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Die Anweisung wurde beendet. (3621)')
-#       change language
-#
+#       simplify following error
+#           Fehler beim EinfÃ¼gen in Test-DB: ('42000', '[42000] [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Zeichenfolgen- oder BinÃ¤rdaten werden in Tabelle "sw_inst.dbo.table_prod", Spalte "name" abgeschnitten. Abgeschnittener Wert: PTV Europe City Map Premium 2022.1H (C:\\Program Files (x86)\\PTVGROUP\\maps\\EuropePremium202. (2628) (SQLExecDirectW); [42000] [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Die Anweisung wurde beendet. (3621)')
+#       dynamic language switch
+#   future changes:
+#       make tables dynamic -> config.ini
+#       MORE EXCEPTIONHANDLING!
+#       make the code more efficient
+#       documentation, maybe even a readme.md file
+#       clean up unused imports
+
+# Would a GUI - at least for windows - make sense?
+# Or should it just be implemented as a service?
 
